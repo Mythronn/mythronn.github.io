29cbd59b25c6bfbf3cf1068844304e9b4101aefc
Mythronn <Mjessek@gmail.com>
2024-10-26 15:16:40 -0400

    adding test martial

---
+++ sappyspellbook/Martial/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM is fully loaded.");
+  });
+  
+  
+  
+  function medievalAlert(message) {
+      const modal = document.getElementById('medievalAlert');
+      const modalBody = modal.querySelector('.modal-body p');
+      modalBody.textContent = message;
+      modal.style.display = 'flex';
+  }
+  document.getElementById('alertCloseButton').addEventListener('click', function() {
+      document.getElementById('medievalAlert').style.display = 'none';
+  });
