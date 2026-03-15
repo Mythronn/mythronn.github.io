b57a64faf69c20a674f098f4ebea2914db1ebc42
Mythronn <Mjessek@gmail.com>
2026-03-14 21:00:43 -0400

    fixing a reset issue in priest

--- sappyspellbook/healer/script.js
+++ sappyspellbook/healer/script.js
@@ -2111,6 +2111,7 @@ function loadSavedList(saveName) {
   if (data.url) {
     const hash = data.url.split("#")[1];
     if (hash) {
+      resetPoints(true);
       window.location.hash = "#" + hash;
       showToast('Loaded "' + saveName + '"');
       console.log("On load saveName: " + saveName);
