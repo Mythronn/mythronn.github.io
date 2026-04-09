1ba841a1778f03364b3d0524f44d61a8745f145a
Mythronn <Mjessek@gmail.com>
2026-04-09 10:20:20 -0400

    testing selection mode in testclass

--- sappyspellbook/zTestClass/script.js
+++ sappyspellbook/zTestClass/script.js
@@ -12,6 +12,9 @@ window.addEventListener('hashchange', function() {
   var referrer = document.referrer;
   //console.log("The referrer is: " + referrer);
   //if (referrer === "" || !referrer.includes(window.location.origin)) 
+    if(selectionMode == false){
+        toggleList();
+    }
     
     parseURL();
     updateTable();;  
@@ -233,7 +236,7 @@ function toggleList(incantOnly){
     levelList[i] = levelList[i].slice(4);
     lists[i].innerHTML = levelList[i];
     if(tables[i].style.display == "block" && incantOnly != 'incants'){
-      
+      selectionMode = false;
       tables[i].style.display = "none";
       lists[i].style.display = "block";
       document.getElementById("saveList").hidden = false;
@@ -263,6 +266,7 @@ function toggleList(incantOnly){
       }
     }
     else if(incantOnly != 'incants'){
+      selectionMode = true;
       tables[i].style.display = "block";
       lists[i].style.display = "none";
       document.getElementById("saveList").hidden = false;
@@ -1052,6 +1056,7 @@ var pointsAvailable = [0, 5,  5,  5,  5,  5,  6];
 var pointsSpent = [0, 0,  0,  0,  0,  0,  0];
 var higherLevelPoints = [0, 0,  0,  0,  0,  0,  0];
 var showIncants = false;
+var selectionMode = true;
 
 /*ABILITY DATA
 |||||||||||||||||||||||||||||||||||
