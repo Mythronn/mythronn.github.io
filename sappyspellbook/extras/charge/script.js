d94459e68d21a395a46d6216db7835506bd4a5a0
Mythronn <Mjessek@gmail.com>
2026-05-06 15:22:36 -0400

    adding charge incant

---
+++ sappyspellbook/extras/charge/script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM is fully loaded.");
+    document.getElementById("buttons").scrollIntoView({ behavior: "smooth" });
+  });
+  
+  function showText(archetype){
+    document.getElementById("Anti-PaladinText").hidden = true; 
+    document.getElementById("CorruptorText").hidden = true;      
+    document.getElementById("InfernalText").hidden = true;     
+    document.getElementById("ArcherText").hidden = true;    
+    document.getElementById("SniperText").hidden = true;  
+    document.getElementById("ArtificerText").hidden = true;     
+    document.getElementById("AssassinText").hidden = true;     
+    document.getElementById("RogueText").hidden = true;   
+    document.getElementById("SpyText").hidden = true;  
+    document.getElementById("BarbarianText").hidden = true;   
+    document.getElementById("RaiderText").hidden = true;    
+    document.getElementById("BerserkerText").hidden = true;     
+    document.getElementById("MonkText").hidden = true;     
+    document.getElementById("MediumText").hidden = true;     
+    document.getElementById("MysticText").hidden = true;   
+    document.getElementById("PaladinText").hidden = true;    
+    document.getElementById("GuardianText").hidden = true;    
+    document.getElementById("InquisitorText").hidden = true;     
+    document.getElementById("ScoutText").hidden = true;     
+    document.getElementById("HunterText").hidden = true;    
+    document.getElementById("ApexText").hidden = true;     
+    document.getElementById("WarriorText").hidden = true;     
+    document.getElementById("MarauderText").hidden = true;    
+    document.getElementById("JuggernautText").hidden = true;
+    
+    document.getElementById(archetype).hidden = false; //<---show
+    console.log(archetype);
+    document.getElementById(archetype).scrollIntoView({ behavior: "smooth" });
+  }
+  
