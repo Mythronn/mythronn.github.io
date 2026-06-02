1170257e3e467b0d7e0fcdd6bc3982dc5b5835f1
Mythronn <Mjessek@gmail.com>
2026-06-02 10:44:55 -0400

    adding update point lables function for clarity

--- sappyspellbook/zTestClass/script.js
+++ sappyspellbook/zTestClass/script.js
@@ -292,7 +292,7 @@ function toggleList(incantOnly){
 /*##################################*/
 function updateTable(){
   var cell;
-  const pointLabels = ["level1Points", "level2Points", "level3Points", "level4Points", "level5Points", "level6Points"];
+  
   for(let i = 0; i < abilityCount; i++){
     cell = document.getElementById(i + "max");
     if(abilities[i].max == 31){
@@ -309,13 +309,18 @@ function updateTable(){
     else
       document.getElementById(abilities[i].pointTotalId).style.background = "";
   }
-  for(i = 0; i < 6; i++){
+   updatePointLabels();
+}
+/*##################################*/
+function updatePointLabels(){
+  const pointLabels = ["level1Points", "level2Points", "level3Points", "level4Points", "level5Points", "level6Points"];
+  for(let i = 0; i < 6; i++){
       if(document.getElementById(pointLabels[i]).value != 0){
         document.getElementById(pointLabels[i]).style.background = "#FFD700";
       }
       else
         document.getElementById(pointLabels[i]).style.background = "";
-  }  
+  } 
 }
 /*##################################*/
 function resetPoints(fullReset){
@@ -352,8 +357,7 @@ function resetPoints(fullReset){
       selectionMode = true;
     }
 
-
-    
+    updatePointLabels();    
     
   }
   processNecromancerMinus();
